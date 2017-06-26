526b15ed05358f809089649f39e8f9e67d40fb55
imjeffparedes <jeffpar0721@outlook.com>
2017-06-26 16:33:28 +0800

    Changed project links

--- simple_weather_display.ino
+++ simple_weather_display.ino
@@ -1,15 +1,15 @@
 
 /**
- * Easy Matrix Weather Display
+ * Weather Matrix Display
  * by Jefferson Paredes
  * Version: 1.0.0
  * 
  * Simple introduction to Internet of Things project 
  * Check this project on Instructables.com
- * Link: https://www.instructables.com/id/Easy-Matrix-Weather-Display
+ * Link: https://www.instructables.com/id/Weather-Matrix-Display
  * 
  * Contribute to improve this project on gist!
- * Link: https://gist.github.com/imjeffparedes/
+ * Link: https://gist.github.com/imjeffparedes/iot-simple-weather-matrix-display
  * 
  * 
  * Need Help?
